547550d7b9c7d3464c2bdf897b55c4641c6a3909
NoorMohammed17 <noormuhammads1797@gmail.com>
2023-08-17 10:45:11 +0530

    email added to form

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -8,18 +8,26 @@ const SimpleInput = (props) => {
     inputBlurHandler: nameBlurHandler,
     reset: resetNameInput,
   } = useInput((value) => value.trim() !== "");
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput((value) => value.includes('@'));
 
 
   let formIsValid = false;
 
-  if (enteredNameIsValid ) {
+  if (enteredNameIsValid  && enteredEmailIsValid) {
     formIsValid = true;
   }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid && !enteredEmailIsValid) {
       return;
     }
 
@@ -27,6 +35,7 @@ const SimpleInput = (props) => {
 
   
     resetNameInput();
+    resetEmailInput();
 
     
   };
@@ -34,6 +43,11 @@ const SimpleInput = (props) => {
   const nameInputClasses = nameInputHasError
   ? 'form-control invalid'
   : 'form-control';
+
+  const emailInputClasses = emailInputHasError
+  ? 'form-control invalid'
+  : 'form-control';
+
   return (
     <form onSubmit={formSubmissionHandler}>
     <div className={nameInputClasses}>
@@ -47,6 +61,19 @@ const SimpleInput = (props) => {
           />
            {nameInputHasError && (
           <p className='error-text'>Name must not be empty.</p>
+        )}
+        </div>
+        <div className={emailInputClasses}>
+          <label htmlFor="name">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={enteredEmail}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+           {emailInputHasError && (
+          <p className='error-text'>Email must not be empty.</p>
         )}
         </div>
         <div className="form-actions">
